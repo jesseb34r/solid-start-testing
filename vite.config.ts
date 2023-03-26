63bc3c585572b3ce4f91ddb9e2428729f3c57b1b
jesseb34r <jessea.login@gmail.com>
2023-03-26 01:05:30 -0700

    feat: deploy to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid(), vanillaExtractPlugin()],
+  plugins: [
+    solid({
+      adapter: vercel({}),
+    }),
+    vanillaExtractPlugin(),
+  ],
   ssr: {
     noExternal: ["@kobalte/core"],
   },
