1bc7b6c369de4bf94ebd838b89d8044bf7ba057a
jesseb34r <jessea.login@gmail.com>
2023-03-26 01:00:58 -0700

    feat(cards): move hovercard logic into cards page
    this allows for the Portal to be rendered with respect to the cursor on the page easily
    remove hover-card.tsx as it is no longer necessary

--- src/routes/cards.tsx
+++ src/routes/cards.tsx
@@ -1,5 +1,5 @@
-import { For } from "solid-js";
-import { HoverCard } from "~/components/hover-card";
+import { createSignal, For, JSX, Show } from "solid-js";
+import { Portal } from "solid-js/web";
 
 export type Card = {
   name: string;
@@ -20,11 +20,75 @@ const mockCards: Card[] = [
   },
 ];
 
+const colorVariants = {
+  W: "bg-[hsl(50,100%,80%,0.2)]",
+  U: "bg-[hsl(220,100%,65%,0.2)]",
+  B: "bg-[hsl(260,40%,50%,0.2)]",
+  R: "bg-[hsl(0,100%,65%,0.2)]",
+  G: "bg-[hsl(100,100%,65%,0.2)]",
+  M: "bg-[hsl(50,100%,40%,0.2)]",
+  C: "bg-[hsl(0,0%,40%,0.2)]",
+  L: "bg-[hsl(20,100%,40%,0.2)]",
+};
+
 export default function CardsPage() {
+  const [hoverCardIsOpen, setHoverCardIsOpen] = createSignal(false);
+  const [hoverCardImageUrl, setHoverCardImageUrl] = createSignal<string>();
+
+  const [cursorX, setCursorX] = createSignal(0);
+  const [cursorY, setCursorY] = createSignal(0);
+  const handlePointerMove: JSX.EventHandlerUnion<HTMLDivElement, PointerEvent> = (e) => {
+    if (e.pointerType === "touch") {
+      return;
+    }
+
+    setCursorX(e.clientX);
+    setCursorY(e.clientY);
+  };
+
   return (
     <main class="flex flex-col items-center justify-center">
-      <div class="p-5 ring ring-white">
-        <For each={mockCards}>{(card) => <HoverCard card={card} />}</For>
+      <div onPointerMove={handlePointerMove} class="flex flex-col overflow-hidden rounded ring ring-mauve6">
+        <For each={mockCards}>
+          {(card) => {
+            const handlePointerEnter: JSX.EventHandlerUnion<HTMLDivElement, PointerEvent> = (e) => {
+              if (e.pointerType === "touch") {
+                return;
+              }
+
+              setHoverCardImageUrl(card.imageUrl);
+              setHoverCardIsOpen(true);
+            };
+
+            const handlePointerLeave: JSX.EventHandlerUnion<HTMLDivElement, PointerEvent> = (e) => {
+              if (e.pointerType === "touch") {
+                return;
+              }
+
+              setHoverCardIsOpen(false);
+            };
+
+            return (
+              <div
+                onPointerEnter={handlePointerEnter}
+                onPointerLeave={handlePointerLeave}
+                onPointerMove={handlePointerMove}
+                class={`${
+                  colorVariants[card.color]
+                } relative cursor-pointer p-1.5 hover:after:absolute hover:after:inset-0 hover:after:bg-whiteA4`}
+              >
+                {card.name}
+              </div>
+            );
+          }}
+        </For>
+        <Show when={hoverCardIsOpen()}>
+          <Portal>
+            <div class="absolute z-50 w-[15rem]" style={{ top: `${cursorY() + 5}px`, left: `${cursorX() + 5}px` }}>
+              <img src={hoverCardImageUrl()} alt="card image" width="262" height="366" class="w-full rounded-xl" />
+            </div>
+          </Portal>
+        </Show>
       </div>
     </main>
   );
