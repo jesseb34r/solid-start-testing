b7807f32c61312a35956cf9a88dd4c6705f832ed
jesseb34r <jessea.login@gmail.com>
2023-03-26 00:29:17 -0700

    add radix colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,31 @@
+const radixColors = require("@radix-ui/colors");
+
+/**
+ * sm = mobile
+ * md = tablet
+ * lg = laptop
+ * xl = ultrawide
+ */
+const screens = {
+  sm: "332px",
+  md: "684px",
+  lg: "1036px",
+  xl: "1388px",
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+    screens,
+    colors: {
+      ...radixColors.sandDark,
+      ...radixColors.orangeDark,
+      ...radixColors.blueDark,
+      ...radixColors.grassDark,
+      ...radixColors.amberDark,
+      ...radixColors.tomatoDark,
+    },
     extend: {},
   },
   plugins: [],
