126cc88b1751bc6e224db39a59e2c206caead5be
jesseb34r <jessea.login@gmail.com>
2023-03-26 01:11:09 -0700

    feat(home): Add home page text

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,9 @@ import { MyTooltip } from "~/components/MyTooltip";
 
 export default function Home() {
   return (
-    <main class="flex flex-col items-center justify-center">
-      <MyTooltip triggerContent={"Hover Me"}>Content</MyTooltip>
+    <main class="flex flex-col items-center">
+      <h1 class="mb-2.5 text-6xl font-semibold tracking-wide">SolidStart Testing</h1>
+      <p class="text-lg text-mauve11">A light project for testing new components in a real life environment</p>
     </main>
   );
 }
